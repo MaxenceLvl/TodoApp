693f1b62ff78eb1ac20cd0e5fe08fdfc9e30edf8
MaxenceLvl <33911862+MaxenceLvl@users.noreply.github.com>
2022-03-04 21:32:48 +0100

    Use data class instead of class for data/model

--- app/src/main/java/com/maxence/data/ToDo.kt
+++ app/src/main/java/com/maxence/data/ToDo.kt
@@ -1,14 +1,13 @@
 package com.maxence.data
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "todo")
-class ToDo(
+data class ToDo(
     val title: String,
     val description: String?,
-    val isComplete: Boolean = false,
+    var isComplete: Boolean = false,
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0) {
 
