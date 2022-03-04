273aa5ce98c01bdc8bf0342823fd897f0d273675
MaxenceLvl <33911862+MaxenceLvl@users.noreply.github.com>
2022-03-04 21:32:22 +0100

    Clean unuse code

--- app/src/main/java/com/maxence/MainActivity.kt
+++ app/src/main/java/com/maxence/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TodoAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -26,18 +25,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TodoAppTheme {
-        Greeting("Android")
     }
 }
