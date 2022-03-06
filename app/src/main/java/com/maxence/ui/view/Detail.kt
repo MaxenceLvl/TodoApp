54678fae06a65374f2da4da6a1a3b7c26a7e1396
MaxenceLvl <33911862+MaxenceLvl@users.noreply.github.com>
2022-03-06 22:38:12 +0100

    Response to Issue 3
    ## 3 : Detail a TODO
    
    ### Description :
    As a user I would like to display one of my todo in a separate or dedicated view.
    This todo will contain its title and a description (which is a new information not shown in the previous view).
    ### Acceptance criterias :
    - We can click on a todo (by any way) to access the details view of the todo
    - The todo could be accessed via a unique URL

--- app/src/main/java/com/maxence/ui/view/Detail.kt
+++ app/src/main/java/com/maxence/ui/view/Detail.kt
@@ -1,22 +1,65 @@
 package com.maxence.ui.view
 
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.maxence.data.TodoViewModel
 
 @Composable
 fun DetailView(todoId: String, viewModel: TodoViewModel) {
     val todo = viewModel.todoList.value?.first { it.id == todoId.toLong() }
-    Scaffold() {
-        
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFBEEFF5))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+                .background(Color.White.copy(alpha = 0.5f)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Title : ",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(start = 6.dp, end = 2.dp)
+                    .padding(vertical = 6.dp)
+            )
+            Text(
+                text = todo!!.title,
+                fontSize = 14.sp,
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+                .background(Color.White.copy(alpha = 0.5f)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Description : ",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(start = 6.dp, end = 2.dp)
+                    .padding(vertical = 6.dp)
+            )
+            Text(
+                text = if (todo?.description != null) todo.description else "No description",
+                fontSize = 14.sp,
+            )
+        }
     }
-    TopAppBar(
-        title = {
-            Text(text = "ToDo Application",color= Color.White)
-        },
-        backgroundColor = Color(0xFFFDA433),
-    )
 }
